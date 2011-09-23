887e29db332127ec39c721696086806f9561e72b
Nora Apsel <nora@meetup.com>
2011-09-23 13:25:16 -0400

    api esk

--- stora/index/views.py
+++ stora/index/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from rooms.models import Room, Reservation
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404, render_to_response
+from django.utils import simplejson
 
 def getAllRooms():
     rooms = Room.objects.all()
@@ -29,26 +30,24 @@ def index(request):
     return render_to_response('index/index.html', {'availableRooms': availableRooms},
                                context_instance=RequestContext(request))
 
-def search(request):
+#    description = models.TextField()
+#    icon = models.TextField(max_length=200)
+#    price = models.IntegerField()
+#    capacity = models.IntegerField()
+#    address = models.CharField(max_length=200)
+#    city = models.CharField(max_length=100)
+#    state = models.CharField(max_length=20)
+#    zip = models.IntegerField()
+#    lat = models.FloatField()
+#    lon = models.FloatField()
+def getDict(room):
+    return {'name':room.name, 'description':room.description, 'icon':room.icon, 'price':room.price, 'capacity':room.capacity, 'address':room.address, 'city':room.city, 'state':room.state, 'zip':room.zip, 'lat':room.lat, 'lon':room.lon}
+
+def api(request):
+    rooms = list(getAllRooms());
+    s = simplejson.dumps([getDict(room) for room in rooms])
+
+    return HttpResponse(s)
     
-    if request.method == "POST":
-        print 'dude'
     
-    zipcode = request.POST['zipcode']
-    date = request.POST['date']
-    capacity = request.POST['capacity']
-    
-    rooms = getAllRooms();
-    
-    availableRooms = [];
-    for room in rooms:
-        r = Reservation.objects.filter(room = room.id)
-        print r
-        if len(r) == 0:
-            print 'no res!'
-            availableRooms.append( room )
-    
-        
-    return render_to_response('index/index.html', {'availableRooms': availableRooms},
-                               context_instance=RequestContext(request))
 
