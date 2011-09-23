b1d24cb898126311f0fdfb0ef990e23b69cbbaa7
Nora Apsel <nora@meetup.com>
2011-09-23 12:12:10 -0400

    make a freaking reservation

--- stora/rooms/views.py
+++ stora/rooms/views.py
@@ -1,10 +1,9 @@
 from django.template import Context, loader
 from django.http import HttpResponse
-from rooms.models import Room
+from rooms.models import Room, Reservation
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404, render_to_response
-import time
-from datetime import date
+import time, datetime
 
 
 def getRoomInfo( room_id ):
@@ -15,16 +14,30 @@ def getRoomInfo( room_id ):
 def index(request, room_id):
 	
 	room = getRoomInfo( room_id )
+	currentReservations = Reservation.objects.filter(room = room.id)
+	status = ""
+	
 	if request.method == "POST":
 		dateF = request.POST['date']
 		durationF = request.POST['duration']
-		sizeF = request.POST['size']
+		sizeF = '10'
 		
-		room.reservation_set.create(res_time=date.today(), duration=durationF, size=sizeF)
+		time_format = "%m/%d/%Y %H:%M"
+		dMan = datetime.datetime.fromtimestamp(time.mktime(time.strptime(dateF, time_format)))
+		
+		for res in currentReservations:
+			if res.res_time == dMan:
+				status = "taken"
+				break;
+
+		if status != "taken":
+			room.reservation_set.create(res_time=dMan, duration=durationF, size=sizeF)
+			status = "posted"
+		
 		
 	
 	
-	return render_to_response('rooms/index.html', {'room': getRoomInfo( room_id )},
+	return render_to_response('rooms/index.html', {'room': room, 'reservations':currentReservations, 'status': status},
 							   context_instance=RequestContext(request))
 
 
