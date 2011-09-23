be9c65ee8cc298ef66e96c7129592cb05df0f836
Nora Apsel <nora@meetup.com>
2011-09-23 14:28:15 -0400

    yeah

--- stora/index/views.py
+++ stora/index/views.py
@@ -4,9 +4,12 @@ from rooms.models import Room, Reservation
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404, render_to_response
 from django.utils import simplejson
+import random
+from django.core import serializers
 
 def getAllRooms():
-    rooms = Room.objects.all()
+    rooms = list(Room.objects.all())
+    random.shuffle(rooms)
     return rooms
 
 def index(request):
@@ -30,24 +33,18 @@ def index(request):
     return render_to_response('index/index.html', {'availableRooms': availableRooms},
                                context_instance=RequestContext(request))
 
-#    description = models.TextField()
-#    icon = models.TextField(max_length=200)
-#    price = models.IntegerField()
-#    capacity = models.IntegerField()
-#    address = models.CharField(max_length=200)
-#    city = models.CharField(max_length=100)
-#    state = models.CharField(max_length=20)
-#    zip = models.IntegerField()
-#    lat = models.FloatField()
-#    lon = models.FloatField()
+
 def getDict(room):
     return {'name':room.name, 'description':room.description, 'icon':room.icon, 'price':room.price, 'capacity':room.capacity, 'address':room.address, 'city':room.city, 'state':room.state, 'zip':room.zip, 'lat':room.lat, 'lon':room.lon}
 
 def api(request):
-    rooms = list(getAllRooms());
+    rooms = getAllRooms();
     s = simplejson.dumps([getDict(room) for room in rooms])
 
-    return HttpResponse(s)
+    data = serializers.serialize('json', Room.objects.all())
+    return HttpResponse(data,'application/javascript')
+    
+#    return HttpResponse(s)
     
     
 
